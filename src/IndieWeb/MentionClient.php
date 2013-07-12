5198d8874b02a4906a5b7d6af4fa344cd4883d3b
pfefferle <pfefferle@gmail.com>
2013-07-12 09:26:53 +0200

    added xmlrpc_encode_request wrapper

--- src/IndieWeb/MentionClient.php
+++ src/IndieWeb/MentionClient.php
@@ -285,3 +285,18 @@ class MentionClient {
   }
 
 }
+
+if (!function_exists('xmlrpc_encode_request')) {
+  function xmlrpc_encode_request($method, $params) {
+    $xml  = '<?xml version="1.0"?>';
+    $xml .= '<methodCall>';
+    $xml .= '<methodName>'.$method.'</methodName>';
+    $xml .= '<params>';
+    foreach ($params as $param) {
+      $xml .= '<param><value><string>'.$param.'</string></value></param>';
+    }
+    $xml .= '</params></methodCall>';
+    
+    return $xml;
+  }
+}
